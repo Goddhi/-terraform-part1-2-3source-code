c6208b74cbad2c688b29f38842dfc3002c00dcd9
Ubuntu <ubuntu@ip-172-31-92-211.ec2.internal>
2023-04-24 12:40:25 +0000

    ami added

--- main.tf
+++ main.tf
@@ -97,9 +97,9 @@ module "RDS" {
 # The Module creates instances for jenkins, sonarqube abd jfrog
 module "compute" {
   source          = "./modules/compute"
-  ami-jenkins     = var.ami
-  ami-sonar       = var.ami
-  ami-jfrog       = var.ami
+  # ami-jenkins     = var.ami
+  ami-sonar       = var.ami-sonar
+  # ami-jfrog       = var.ami
   subnets-compute = module.VPC.public_subnets-1
   sg-compute      = [module.Security.Ext-ALB-sg]
   keypair         = var.keypair
