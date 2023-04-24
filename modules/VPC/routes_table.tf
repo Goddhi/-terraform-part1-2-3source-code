03b453e62b3d1ee8914c90b71d470aef50ed6fad
Ubuntu <ubuntu@ip-172-31-92-211.ec2.internal>
2023-04-24 07:19:54 +0000

    code changed

--- modules/VPC/routes_table.tf
+++ modules/VPC/routes_table.tf
@@ -11,12 +11,7 @@ resource "aws_route_table" "private-rtb" {
 }
 
 
-# associate all private subnets to the private route table
-# resource "aws_route_table_association" "private-subnets-assoc" {
-#   count          = length(aws_subnet.private[*].id)  # returns the length of all priate subnets
-#   subnet_id      = element(aws_subnet.private[*].id, count.index) # the element function starts counting from 0 and attach each priate subnet at each iteration
-#   route_table_id = aws_route_table.private-rtb.id
-# }
+
 
 # Create route for the private route table and attach a nat gateway to it
 resource "aws_route" "private-rtb-route" {
@@ -25,12 +20,19 @@ resource "aws_route" "private-rtb-route" {
   gateway_id             = aws_nat_gateway.nat.id
 }
 
-
+# associate all private subnets to the private route table
 resource "aws_route_table_association" "private-subnets-assoc" {
-  for_each = toset(aws_subnet.private[*].id)
-  subnet_id      = each.value
+  count          = length(aws_subnet.private[*].id)  # returns the length of all priate subnets
+  subnet_id      = element(aws_subnet.private[*].id, count.index) # the element function starts counting from 0 and attach each priate subnet at each iteration
   route_table_id = aws_route_table.private-rtb.id
 }
+
+
+# resource "aws_route_table_association" "private-subnets-assoc" {
+#   for_each = toset(aws_subnet.private[*].id)
+#   subnet_id      = each.value
+#   route_table_id = aws_route_table.private-rtb.id
+# }
 
 
 # create route table for the public subnets
@@ -52,15 +54,15 @@ resource "aws_route" "public-rtb-route" {
 }
 
 # associate all public subnets to the public route table
-# resource "aws_route_table_association" "public-subnets-assoc" {
-#   count          = length(aws_subnet.public[*].id)
-#   subnet_id      = element(aws_subnet.public[*].id, count.index)
-#   route_table_id = aws_route_table.public-rtb.id
-# }
-
 resource "aws_route_table_association" "public-subnets-assoc" {
-  for_each = toset(aws_subnet.public[*].id)
-  subnet_id      = each.value
+  count          = length(aws_subnet.public[*].id)
+  subnet_id      = element(aws_subnet.public[*].id, count.index)
   route_table_id = aws_route_table.public-rtb.id
 }
+
+# resource "aws_route_table_association" "public-subnets-assoc" {
+#   for_each = toset(aws_subnet.public[*].id)
+#   subnet_id      = each.value
+#   route_table_id = aws_route_table.public-rtb.id
+# }
 
