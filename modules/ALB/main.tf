304d76947d9e6cf6833d38f2862d0c9a9791ee55
Ubuntu <ubuntu@ip-172-31-92-211.ec2.internal>
2023-04-24 13:18:15 +0000

    healt check issue fixed

--- modules/ALB/main.tf
+++ modules/ALB/main.tf
@@ -41,17 +41,17 @@ resource "aws_lb_target_group" "nginx-tg" {
 }
 
 # Create listener to redirect traffic to the target group.
-resource "aws_lb_listener" "nginx-listener" {
-  load_balancer_arn = aws_lb.ext-alb.arn
-  port              = var.port # 80
-  protocol          = var.protocol # "HTTP"
-  # certificate_arn   = aws_acm_certificate_validation.drexii.certificate_arn
+# resource "aws_lb_listener" "nginx-listener" {
+#   load_balancer_arn = aws_lb.ext-alb.arn
+#   port              = var.port # 80
+#   protocol          = var.protocol # "HTTP"
+#   # certificate_arn   = aws_acm_certificate_validation.drexii.certificate_arn
 
-  default_action {
-    type             = var.default_action_type # "forward"
-    target_group_arn = aws_lb_target_group.nginx-tg.arn
-  }
-}
+#   default_action {
+#     type             = var.default_action_type # "forward"
+#     target_group_arn = aws_lb_target_group.nginx-tg.arn
+#   }
+# }
 
 #Internal Load Balancers for webservers
 resource "aws_lb" "int-alb" {
@@ -115,31 +115,31 @@ resource "aws_lb_target_group" "tooling-tg" {
 
 # For this aspect a single listener was created for the wordpress which is default,
 # A rule was created to route traffic to tooling when the host header changes
-resource "aws_lb_listener" "web-listener" {
-  load_balancer_arn = aws_lb.int-alb.arn
-  port              = var.port # 80
-  protocol          = var.protocol # "HTTP"
-  # certificate_arn   = aws_acm_certificate_validation.drexii.certificate_arn
+# resource "aws_lb_listener" "web-listener" {
+#   load_balancer_arn = aws_lb.int-alb.arn
+#   port              = var.port # 80
+#   protocol          = var.protocol # "HTTP"
+#   # certificate_arn   = aws_acm_certificate_validation.drexii.certificate_arn
 
-  default_action {
-    type             = var.default_action_type # "forward"
-    target_group_arn = aws_lb_target_group.wordpress-tg.arn
-  }
-}
+#   default_action {
+#     type             = var.default_action_type # "forward"
+#     target_group_arn = aws_lb_target_group.wordpress-tg.arn
+#   }
+# }
 
 # listener rule for tooling target
-resource "aws_lb_listener_rule" "tooling-listener" {
-  listener_arn = aws_lb_listener.web-listener.arn
-  priority     = var.priority # 99
+# resource "aws_lb_listener_rule" "tooling-listener" {
+#   listener_arn = aws_lb_listener.web-listener.arn
+#   priority     = var.priority # 99
 
-  action {
-    type             = var.default_action_type # "forward"
-    target_group_arn = aws_lb_target_group.tooling-tg.arn
-  }
+#   action {
+#     type             = var.default_action_type # "forward"
+#     target_group_arn = aws_lb_target_group.tooling-tg.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["tooling.drexii.online"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["tooling.drexii.online"]
+#     }
+#   }
+# }
